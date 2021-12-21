351e042dfbdb93266e90e48bd8b55b32e056ac7a
Jonathan Miller <jon@jmill.dev>
2021-12-21 08:17:24 -0500

    Finish section 1

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,27 @@
+import { useEffect, useMemo, useState } from "react";
+
+import { useWeb3 } from "@3rdweb/hooks";
+
 const App = () => {
+  const { connectWallet, address, error, provider } = useWeb3();
+  console.log("Address: ", address);
+
+  if (!address) {
+    return (
+      <div className="landing">
+        <h1>Welcome to DataSciDAO</h1>
+        <button className="btn-hero" onClick={() => connectWallet("injected")}>
+          Connect your wallet
+        </button>
+      </div>
+    );
+  }
+
   return (
     <div className="landing">
-      <h1>Welcome to My DAO</h1>
+      <h1>Wallet Connected, now what!</h1>
     </div>
-  );
+  )
 };
 
 export default App;
